76a60200f690ed91bcd8d1baad6a74709d8e41d4
nicolesorial <sorial.n@northeastern.edu>
2023-04-17 18:23:51 -0400

    create new quiz

--- flask-app/src/writers/writers.py
+++ flask-app/src/writers/writers.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
-from src import db
+from src import db, app as current_app
 
 
 writers = Blueprint('writers', __name__)
 
+# Helper function to execute DB cursor, for use when DB needs to be changed
 def execute_cursor(sql_query):
+    cursor = db.get_db().cursor()
+    cursor.execute(sql_query)
+    db.get_db().commit()
+
+# Helper function to execute DB cursor, for use when DB does not need to be changed
+def execute_cursor_with_response(sql_query):
     cursor = db.get_db().cursor()
     cursor.execute(sql_query)    
     row_headers = [x[0] for x in cursor.description]
@@ -25,13 +32,38 @@ def get_writers():
         SELECT id, username, FName, LName, yearsActive
         FROM Writer
     '''
-    return execute_cursor(query)
+    return execute_cursor_with_response(query)
 
-# Create a quiz and its associated questions
+# Creates a new quiz
 @writers.route('/quizzes', methods=['POST'])
 def create_quiz():
-    # TODO
-    return nil
+    # collecting data from request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extracting the vars
+    status = the_data['status']
+    category = the_data['category']
+    numOffenses = the_data['numOffenses']
+    url = the_data['url']
+    writerId = the_data['writerId']
+    title = the_data['title']
+    description = the_data['description']
+
+    # constructing the query
+    query = '''
+        INSERT INTO Quiz (status, category, numOffenses, url, writerId, title, description)
+        VALUES (#{status}, #{category}, #{numOffenses}, #{url}, #{writerId}, #{title}, #{description})
+    '''
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement
+    execute_cursor(query)
+
+    return 'Success!'
+
+# Creates a new question for a quiz
+# TODO
 
 # Get statistics behind a particular quiz
 # TODO
