c7f18f323001607354ff89e5db59cf7a9c9f14b6
Logan Morneault <loganmorneault@gmail.com>
2023-04-18 18:45:21 -0400

    Comments blueprint

--- flask-app/src/comments/comments.py
+++ flask-app/src/comments/comments.py
@@ -1,16 +1,55 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, make_response, request, jsonify, Response
 from src import db
 
 comments = Blueprint('comments', __name__)
 
-@comments.route('/comments/<comment_id>', methods=['GET'])
-def get_comment():
-    pass
+# Gets a comment with the given CommentID
+@comments.route('/comments/<int:comment_id>', methods=['GET'])
+def get_comment(comment_id: int) -> Response:
+    # Get data from DB
+    cursor = db.get_db().cursor()
+    cursor.execute(f'''
+    SELECT * FROM ThoughtComments
+    WHERE CommentID = {comment_id};
+    ''')
 
-@comments.route('/comments/<comment_id>', methods=['PUT'])
-def update_comment():
-    pass
+    # JSONify response and return
+    column_headers = [x[0] for x in cursor.description]
+    json_data = [dict(zip(column_headers, row)) for row in cursor.fetchall()]
+    return jsonify(json_data)
 
-@comments.route('/comments/<comment_id>', methods=['DELETE'])
-def delete_comment():
-    pass
+
+# Updates a comment with the given CommentID
+@comments.route('/comments/<int:comment_id>', methods=['PUT'])
+def update_comment(comment_id: int) -> Response:
+    try:
+        # Update data in DB
+        cursor = db.get_db().cursor()
+        cursor.execute(f'''
+        UPDATE ThoughtComments
+        SET Content = {request.form['content']}
+        WHERE CommentID = {comment_id}
+        ''')
+        
+        # Return OK
+        return make_response(200)
+    except: 
+        # Return error
+        return make_response(400)
+
+
+# Deletes a comment with the given CommentID
+@comments.route('/comments/<int:comment_id>', methods=['DELETE'])
+def delete_comment(comment_id: int):
+    try:
+        # Update data in DB
+        cursor = db.get_db().cursor()
+        cursor.execute(f'''
+        DELETE FROM ThoughtComments
+        WHERE CommentID = {comment_id}
+        ''')
+
+        # Return OK
+        return make_response(200)
+    except:
+        return make_response(400)
